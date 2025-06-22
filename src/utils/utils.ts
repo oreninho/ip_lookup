beb5d69f3fc1d875ba5435bd1247b7c4046fcc8f
oreninho <oreninho@gmail.com>
2025-06-22 14:46:30 +0300

    fix format date function to use the correct option key

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,12 +1,8 @@
-// src/utils/ipValidation.ts
 export const IPV4_REGEX =
     /^(?:25[0-5]|2[0-4]\d|1\d\d|\d?\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|\d?\d)){3}$/;
 
 export function validateIp(ip: string): boolean {
     return IPV4_REGEX.test(ip);
-}
-export function validatePort(port: number): boolean {
-    return port >= 0 && port <= 65535;
 }
 
 export function formatTimeInZone({date = new Date(), timezone,config= { hour: '2-digit',
@@ -16,7 +12,7 @@ export function formatTimeInZone({date = new Date(), timezone,config= { hour: '2
                                      { date?: Date; timezone: string }): string
 {
     return date.toLocaleTimeString('en-US', {
-        timezone,
+        timeZone: timezone,
         ...config
     });
 }
